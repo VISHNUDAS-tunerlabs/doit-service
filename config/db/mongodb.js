55ff167898fe8055d5816665664b3efcd6ba6144
vishnu <vishnu@tunerlabs.com>
2024-10-30 23:09:34 +0530

    globals file changes

--- config/db/mongodb.js
+++ config/db/mongodb.js
@@ -6,38 +6,34 @@
  */
 
 // dependencies
-const mongoose = require("mongoose");
-const mongoose_delete = require("mongoose-delete");
-const mongoose_autopopulate = require("mongoose-autopopulate");
-const mongoose_timestamp = require("mongoose-timestamp");
-const mongoose_ttl = require("mongoose-ttl");
+const mongoose = require('mongoose');
+const mongoose_delete = require('mongoose-delete');
+const mongoose_autopopulate = require('mongoose-autopopulate');
+const mongoose_timestamp = require('mongoose-timestamp');
+const mongoose_ttl = require('mongoose-ttl');
 const ObjectId = mongoose.Types.ObjectId;
 
 /**
  * Mongodb setup.
  * @method
  * @name DB
-*/
+ */
 
 const DB = function () {
+  const db = mongoose.createConnection(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
 
-  const db = mongoose.createConnection(
-    process.env.MONGODB_URL,
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
-    }
-  );
-
-  db.on("error", console.error.bind(console, "connection error:"));
-  db.once("open", function () {
-    console.log("Connected to DB");
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function () {
+    console.log('Connected to DB');
   });
 
   const createModel = function (opts) {
-    if (typeof opts.schema.__proto__.instanceOfSchema === "undefined") {
+    if (typeof opts.schema.__proto__.instanceOfSchema === 'undefined') {
       var schema = mongoose.Schema(opts.schema, opts.options);
     } else {
       var schema = opts.schema;
@@ -45,21 +41,18 @@ const DB = function () {
 
     // apply Plugins
     schema.plugin(mongoose_timestamp, {
-      createdAt: "createdAt",
-      updatedAt: "updatedAt"
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt',
     });
     schema.plugin(mongoose_autopopulate);
     schema.plugin(mongoose_delete, { overrideMethods: true, deletedAt: true });
 
     // Expire at
     if (opts.options) {
-      if (
-        opts.options.expireAfterSeconds ||
-        opts.options.expireAfterSeconds === 0
-      ) {
-        log.debug("Expire Configured for " + opts.name);
+      if (opts.options.expireAfterSeconds || opts.options.expireAfterSeconds === 0) {
+        log.debug('Expire Configured for ' + opts.name);
         schema.plugin(mongoose_ttl, {
-          ttl: opts.options.expireAfterSeconds * 1000
+          ttl: opts.options.expireAfterSeconds * 1000,
         });
       }
     }
@@ -82,7 +75,7 @@ const DB = function () {
     createModel: createModel,
     ObjectId: ObjectId,
     models: db.models,
-    runCompoundIndex: runCompoundIndex
+    runCompoundIndex: runCompoundIndex,
   };
 };
 
